cb09141f63c33144ca6b45d12ca0237e07367d95
Alex Da S A Oliveira <mzt.asaoliveira@voegol.com.br>
2024-03-16 20:04:02 -0300

    Domain Validation: NotNullOrEmpty and Domain Validation: MinLength

--- tests/FC.Codeflix.Catalog.UnitTests/Domain/Validation/DomainValidationTest.cs
+++ tests/FC.Codeflix.Catalog.UnitTests/Domain/Validation/DomainValidationTest.cs
@@ -12,10 +12,10 @@ public class DomainValidationTest
     [Trait("Domain", "DomainValidation - Validation")]
     public void NotNullOK()
     {
-        var value = Faker.Commerce.ProductName();
+        var target = Faker.Commerce.ProductName();
 
         Action action = 
-            () => DomainValidation.NotNull(value, "Value");
+            () => DomainValidation.NotNull(target, "FieldName");
         action.Should().NotThrow();
     }
 
@@ -23,17 +23,71 @@ public class DomainValidationTest
     [Trait("Domain", "DomainValidation - Validation")]
     public void NotNullThrowWhenNull()
     {
-        string? value = null;
+        string? target = null;
 
         Action action =
-            () => DomainValidation.NotNull(value, "FieldName");
+            () => DomainValidation.NotNull(target, "FieldName");
 
         action
             .Should()
             .Throw<EntityValidationException>()
             .WithMessage("FieldName should not be null");
     }
+
     // nao ser null ou vazio
+    [Fact(DisplayName = nameof(NotNullOrEmptyOK))]
+    [Trait("Domain", "DomainValidation - Validation")]
+    public void NotNullOrEmptyOK()
+    {
+        var target = Faker.Commerce.ProductName();
+
+        Action action =
+            () => DomainValidation.NotNullOrEmpty(target, "FieldName");
+        action.Should().NotThrow();
+    }
+
+    [Theory(DisplayName = nameof(NotNullOrEmptyThrowWhenEmpty))]
+    [Trait("Domain", "DomainValidation - Validation")]
+    [InlineData("")]
+    [InlineData(" ")]
+    [InlineData(null)]
+    public void NotNullOrEmptyThrowWhenEmpty(string? target)
+    {
+        Action action =
+            () => DomainValidation.NotNullOrEmpty(target, "FieldName");
+
+        action
+            .Should()
+            .Throw<EntityValidationException>()
+            .WithMessage("FieldName should not be null or empty");
+    }
     // tamanho minimo
+    [Theory(DisplayName = nameof(MinLengthThrowWhenLess))]
+    [Trait("Domain", "DomainValidation - Validation")]
+    [MemberData(nameof(GetValuesSmallerThanTheMin), parameters: 10)]
+    public void MinLengthThrowWhenLess(string target, int minLength)
+    {
+        Action action =
+            () => DomainValidation.MinLength(target, minLength, "FieldName");
+
+        action
+            .Should()
+            .Throw<EntityValidationException>()
+            .WithMessage($"FieldName should not be less than {minLength} characters long");
+    }
+
+    private static IEnumerable<object[]> GetValuesSmallerThanTheMin(int numbersOfTests = 5)
+    {
+        yield return new object[] { "12345", 10 };
+
+        var faker = new Faker();
+
+        for (int i = 0; i < numbersOfTests; i++)
+        {
+            var example = faker.Commerce.ProductName();
+            var minLength = example.Length + (new Random()).Next(1, 20);
+            yield return new object[] { example, minLength };
+        }
+    }
     // tamanho maximo
 }
