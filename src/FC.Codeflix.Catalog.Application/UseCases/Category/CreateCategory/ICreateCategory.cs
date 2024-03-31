62bbaf6c025fc4b67c0e88975aa6808e7194c15e
Alex Da S A Oliveira <mzt.asaoliveira@voegol.com.br>
2024-03-31 16:32:27 -0300

    Contrato Unit Of Work - implementando o Use Case CreateCategory

---
+++ src/FC.Codeflix.Catalog.Application/UseCases/Category/CreateCategory/ICreateCategory.cs
@@ -0,0 +1,6 @@
+﻿namespace FC.Codeflix.Catalog.Application.UseCases.Category.CreateCategory;
+public interface ICreateCategory
+{
+    //Também usado Execute ou Process
+    public Task<CreateCategoryOutput> Handle(CreateCategoryInput input, CancellationToken cancellationToken);
+}
