bb311d9efa147e6517683e152e34c7b9ec66b0f6
Alex Da S A Oliveira <mzt.asaoliveira@voegol.com.br>
2024-04-02 19:48:43 -0300

    Refactoring do teste

--- tests/FC.Codeflix.Catalog.UnitTests/Application/CreateCategory/CreateCategoryTest.cs
+++ tests/FC.Codeflix.Catalog.UnitTests/Application/CreateCategory/CreateCategoryTest.cs
@@ -1,31 +1,32 @@
-﻿using FC.Codeflix.Catalog.Application.Interfaces;
-using FC.Codeflix.Catalog.Domain.Entity;
-using FC.Codeflix.Catalog.Domain.Repository;
+﻿using FC.Codeflix.Catalog.Domain.Entity;
 using FluentAssertions;
 using Moq;
-using System.Threading;
 using Xunit;
 using UseCases = FC.Codeflix.Catalog.Application.UseCases.Category.CreateCategory;
 
 namespace FC.Codeflix.Catalog.UnitTests.Application.CreateCategory;
 
-public  class CreateCategoryTest
+[Collection(nameof(CreateCategoryTestFixture))]
+public class CreateCategoryTest
 {
+    private readonly CreateCategoryTestFixture _fixture;
+
+    public CreateCategoryTest(
+        CreateCategoryTestFixture fixture)
+        => _fixture = fixture;
+    
+
     [Fact(DisplayName = nameof(CreateCategory))]
     [Trait("Application","CreateCategory - Use Cases")]
     public async void CreateCategory()
     {
-        var repositoryMock = new Mock<ICategoryRepository>();
-        var unitOfWorkMock = new Mock<IUnitOfWork>();
+        var repositoryMock = _fixture.GetRepositoryMock();
+        var unitOfWorkMock = _fixture.GetUnitOfWorkMock();
         var useCase = new UseCases.CreateCategory(
             repositoryMock.Object,
             unitOfWorkMock.Object
         );
-        var input = new UseCases.CreateCategoryInput(
-            "Category Name",
-            "Category Description",
-            true
-        );
+        var input = _fixture.GetInput();
         var output = await useCase.Handle(input, CancellationToken.None);
 
         repositoryMock.Verify(
@@ -42,13 +43,12 @@ public  class CreateCategoryTest
         );
 
         output.Should().NotBeNull();
-        output.Name.Should().Be("Category Name");
-        output.Description.Should().Be("Category Description");
-        output.IsActive.Should().BeTrue();
+        output.Name.Should().Be(input.Name);
+        output.Description.Should().Be(input.Description);
+        output.IsActive.Should().Be(input.IsActive);
         output.Id.Should().NotBeEmpty();
         output.CreatedAt.Should().NotBeSameDateAs(default(DateTime));
         //(output.Id != Guid.Empty).Should().BeTrue();
         //(output.CreatedAt != default(DateTime)).Should().BeTrue();
-
     }
 }
