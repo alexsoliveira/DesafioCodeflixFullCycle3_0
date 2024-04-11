5d59080084b1b26d8440a1628fa4da1089cc8b66
Alex Da S A Oliveira <mzt.asaoliveira@voegol.com.br>
2024-04-10 21:26:36 -0300

    Refactoring do use case com MediatR

--- src/FC.Codeflix.Catalog.Application/UseCases/Category/CreateCategory/ICreateCategory.cs
+++ src/FC.Codeflix.Catalog.Application/UseCases/Category/CreateCategory/ICreateCategory.cs
@@ -1,5 +1,8 @@
-﻿namespace FC.Codeflix.Catalog.Application.UseCases.Category.CreateCategory;
-public interface ICreateCategory
+﻿using MediatR;
+
+namespace FC.Codeflix.Catalog.Application.UseCases.Category.CreateCategory;
+public interface ICreateCategory 
+    : IRequestHandler<CreateCategoryInput,CreateCategoryOutput>
 {
     //Também usado Execute ou Process
     public Task<CreateCategoryOutput> Handle(CreateCategoryInput input, CancellationToken cancellationToken);
