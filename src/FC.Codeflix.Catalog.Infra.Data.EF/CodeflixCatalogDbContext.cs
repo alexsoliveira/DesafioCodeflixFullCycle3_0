27dbbdce82a23d208bc2b6f75a60ea89c7252b33
alexsoliveira <alexsoliveira@gmail.com>
2024-06-10 12:02:48 -0300

    Criando teste do Insert do repositório CategoryRepository, Criando o contexto e mapeamentos e Implementando o Insert do CategoryRepository

---
+++ src/FC.Codeflix.Catalog.Infra.Data.EF/CodeflixCatalogDbContext.cs
@@ -0,0 +1,22 @@
+﻿using FC.Codeflix.Catalog.Domain.Entity;
+using FC.Codeflix.Catalog.Infra.Data.EF.Configurations;
+using Microsoft.EntityFrameworkCore;
+using System.Reflection;
+
+namespace FC.Codeflix.Catalog.Infra.Data.EF
+{
+    public class CodeflixCatalogDbContext
+        : DbContext
+    {
+        public DbSet<Category> Categories => Set<Category>();
+        public CodeflixCatalogDbContext(
+            DbContextOptions<CodeflixCatalogDbContext> options)
+            : base(options) { }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            //modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+            modelBuilder.ApplyConfiguration(new CategoryConfiguration());
+        }
+    }
+}
--- tests/FC.Codeflix.Catalog.IntegrationTests/Infra.Data.EF/Repositories/CategoryRepository/CategoryRepositoryTestFixture.cs
+++ tests/FC.Codeflix.Catalog.IntegrationTests/Infra.Data.EF/Repositories/CategoryRepository/CategoryRepositoryTestFixture.cs
@@ -1,4 +1,7 @@
-﻿using FC.Codeflix.Catalog.IntegrationTests.Base;
+﻿using FC.Codeflix.Catalog.Domain.Entity;
+using FC.Codeflix.Catalog.Infra.Data.EF;
+using FC.Codeflix.Catalog.IntegrationTests.Base;
+using Microsoft.EntityFrameworkCore;
 using Xunit;
 
 namespace FC.Codeflix.Catalog.IntegrationTests.Infra.Data.EF.Repositories.CategoryRepository
@@ -11,5 +14,45 @@ namespace FC.Codeflix.Catalog.IntegrationTests.Infra.Data.EF.Repositories.Catego
     public class CategoryRepositoryTestFixture
         : BaseFixture
     {
+        public string GetValidCategoryName()
+        {
+            var categoryName = "";
+
+            while (categoryName.Length < 3)
+                categoryName = Faker.Commerce.Categories(1)[0];
+
+            if (categoryName.Length > 255)
+                categoryName = categoryName[..255];
+
+            return categoryName;
+        }
+
+        public string GetValidCategoryDescription()
+        {
+            var categoryDescription =
+                Faker.Commerce.ProductDescription();
+
+            if (categoryDescription.Length > 10_000)
+                categoryDescription
+                    = categoryDescription[..10_000];
+
+            return categoryDescription;
+        }
+
+        public bool GetRandomBoolean()
+            => new Random().NextDouble() > 0.5;
+        public Category GetExampleCategory()
+        => new(
+            GetValidCategoryName(),
+            GetValidCategoryDescription(),
+            GetRandomBoolean()
+        );
+
+        public CodeflixCatalogDbContext CreateDbContext()
+            => new CodeflixCatalogDbContext(
+                new DbContextOptionsBuilder<CodeflixCatalogDbContext>()
+                .UseInMemoryDatabase("integration-texts-db")
+                .Options
+            );
     }
 }
