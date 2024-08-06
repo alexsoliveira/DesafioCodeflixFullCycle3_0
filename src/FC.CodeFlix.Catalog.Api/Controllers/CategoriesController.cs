e438d788474598a5a96e4df1de9c3504bf911e48
alexsoliveira <alexsoliveira@gmail.com>
2024-08-06 17:19:53 -0300

    Implementação buscar categoria pelo id

--- src/FC.CodeFlix.Catalog.Api/Controllers/CategoriesController.cs
+++ src/FC.CodeFlix.Catalog.Api/Controllers/CategoriesController.cs
@@ -1,5 +1,6 @@
 using FC.Codeflix.Catalog.Application.UseCases.Category.Common;
 using FC.Codeflix.Catalog.Application.UseCases.Category.CreateCategory;
+using FC.Codeflix.Catalog.Application.UseCases.Category.GetCategory;
 using MediatR;
 using Microsoft.AspNetCore.Mvc;
 
@@ -13,7 +14,7 @@ namespace FC.CodeFlix.Catalog.Api.Controllers
 
         public CategoriesController(IMediator mediator)
             => _mediator = mediator;
-        
+
         [HttpPost]
         [ProducesResponseType(typeof(CategoryModelOutput), StatusCodes.Status201Created)]
         [ProducesResponseType(typeof(ProblemDetails), StatusCodes.Status400BadRequest)]
@@ -21,10 +22,21 @@ namespace FC.CodeFlix.Catalog.Api.Controllers
         public async Task<IActionResult> Create(
             [FromBody] CreateCategoryInput input,
             CancellationToken cancellationToken
-        ){
+        ) {
             var output = await _mediator.Send(input, cancellationToken);
             return CreatedAtAction(
                 nameof(Create), new { output.Id }, output);
+        }
+
+        [HttpGet("{id:guid}")]
+        [ProducesResponseType(typeof(CategoryModelOutput), StatusCodes.Status200OK)]        
+        public async Task<IActionResult> GetById(
+           [FromRoute] Guid id,
+           CancellationToken cancellationToken
+       )
+        {
+            var output = await _mediator.Send(new GetCategoryInput(id), cancellationToken);
+            return Ok(output);
         }
     }
 }
