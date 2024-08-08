d17a8766b1edd923ededeb26a3f8131e945e69b5
alexsoliveira <alexsoliveira@gmail.com>
2024-08-08 16:48:23 -0300

    Teste do endpoint de update da category

--- tests/FC.Codeflix.Catalog.EndToEndTests/Base/ApiClient.cs
+++ tests/FC.Codeflix.Catalog.EndToEndTests/Base/ApiClient.cs
@@ -1,5 +1,4 @@
-﻿using Microsoft.VisualStudio.TestPlatform.Utilities;
-using System.Text;
+﻿using System.Text;
 using System.Text.Json;
 
 namespace FC.Codeflix.Catalog.EndToEndTests.Base
@@ -25,15 +24,7 @@ namespace FC.Codeflix.Catalog.EndToEndTests.Base
                     "application/json"
                 )
             );
-            var outputString = await response.Content.ReadAsStringAsync();
-            TOutput? output = null;
-            if(!string.IsNullOrWhiteSpace(outputString))            
-                output = JsonSerializer.Deserialize<TOutput>(outputString,
-                    new JsonSerializerOptions
-                    {
-                        PropertyNameCaseInsensitive = true
-                    }
-                );                        
+            var output = await GetOutput<TOutput>(response);
             return (response, output);
         }
 
@@ -43,15 +34,7 @@ namespace FC.Codeflix.Catalog.EndToEndTests.Base
             where TOutput : class
         {
             var response = await _httpClient.GetAsync(route);
-            var outputString = await response.Content.ReadAsStringAsync();
-            TOutput? output = null;
-            if (!string.IsNullOrWhiteSpace(outputString))
-                output = JsonSerializer.Deserialize<TOutput>(outputString,
-                    new JsonSerializerOptions
-                    {
-                        PropertyNameCaseInsensitive = true
-                    }
-                );
+            var output = await GetOutput<TOutput>(response);
             return (response, output);
         }
 
@@ -61,6 +44,31 @@ namespace FC.Codeflix.Catalog.EndToEndTests.Base
             where TOutput : class
         {
             var response = await _httpClient.DeleteAsync(route);
+            var output = await GetOutput<TOutput>(response);
+            return (response, output);
+        }
+
+        public async Task<(HttpResponseMessage?, TOutput?)> Put<TOutput>(
+            string route,
+            object payload
+        )
+            where TOutput : class
+        {
+            var response = await _httpClient.PutAsync(
+                route,
+                new StringContent(
+                    JsonSerializer.Serialize(payload),
+                    Encoding.UTF8,
+                    "application/json"
+                )
+            );
+            var output = await GetOutput<TOutput>(response);
+            return (response, output);
+        }
+
+        private async Task<TOutput?> GetOutput<TOutput> (HttpResponseMessage response)
+            where TOutput : class
+        {
             var outputString = await response.Content.ReadAsStringAsync();
             TOutput? output = null;
             if (!string.IsNullOrWhiteSpace(outputString))
@@ -70,7 +78,7 @@ namespace FC.Codeflix.Catalog.EndToEndTests.Base
                         PropertyNameCaseInsensitive = true
                     }
                 );
-            return (response, output);
+            return output;
         }
     }
 }
