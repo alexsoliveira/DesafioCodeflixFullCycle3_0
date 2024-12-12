1038e81db8000a7cccbbda5bb1f2664b58f053d4
alexsoliveira <alexsoliveira@gmail.com>
2024-12-12 18:51:34 -0300

    Verificando exceções de domínio no CreateGenre

--- tests/FC.Codeflix.Catalog.UnitTests/Application/Genre/CreateGenre/CreateGenreTest.cs
+++ tests/FC.Codeflix.Catalog.UnitTests/Application/Genre/CreateGenre/CreateGenreTest.cs
@@ -1,5 +1,6 @@
 ﻿using FC.Codeflix.Catalog.Application.Exceptions;
 using FC.Codeflix.Catalog.Application.UseCases.Category.CreateCategory;
+using FC.Codeflix.Catalog.Domain.Exceptions;
 using FluentAssertions;
 using Moq;
 using Xunit;
@@ -130,5 +131,29 @@ public class CreateGenreTest
                 It.IsAny<CancellationToken>()
             ),Times.Once
         );
+    }
+
+    [Theory(DisplayName = nameof(ThrowWhenNameIsInvalid))]
+    [Trait("Application", "CreateGenre - Use Cases")]
+    [InlineData("")]
+    [InlineData(null)]
+    [InlineData("  ")]
+    public async Task ThrowWhenNameIsInvalid(string name)
+    {
+        var input = _fixture.GetExampleInput(name);        
+        var genreRepositoryMock = _fixture.GetGenreRepositoryMock();
+        var categoryRepositoryMock = _fixture.GetCategoryRepositoryMock();
+        var unitOfWorkMock = _fixture.GetUnitOfWorkMock();        
+        var useCase = new UseCase.CreateGenre(
+            genreRepositoryMock.Object,
+            unitOfWorkMock.Object,
+            categoryRepositoryMock.Object
+        );
+
+        var action = async ()
+            => await useCase.Handle(input, CancellationToken.None);
+
+        await action.Should().ThrowAsync<EntityValidationException>()
+            .WithMessage($"Name should not be empty or null");        
     }
 }
--- tests/FC.Codeflix.Catalog.UnitTests/Application/Genre/CreateGenre/CreateGenreTestFixture.cs
+++ tests/FC.Codeflix.Catalog.UnitTests/Application/Genre/CreateGenre/CreateGenreTestFixture.cs
@@ -21,6 +21,12 @@ public class CreateGenreTestFixture
             GetRandomBoolean()            
         );
 
+    public CreateGenreInput GetExampleInput(string? name)
+        => new CreateGenreInput(
+            name!,
+            GetRandomBoolean()
+        );
+
     public CreateGenreInput GetExampleInputWithCategories()
     {
         var numberOfCategoriesIds = (new Random()).Next(1,10);
