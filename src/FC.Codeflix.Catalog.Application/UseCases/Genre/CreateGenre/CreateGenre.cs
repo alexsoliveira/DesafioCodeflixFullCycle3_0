86d1b3bc1a5816bdbe4bcd806b956052e1316f93
alexsoliveira <alexsoliveira@gmail.com>
2024-12-12 18:36:58 -0300

    Refactoring da verificação dos IDs

--- src/FC.Codeflix.Catalog.Application/UseCases/Genre/CreateGenre/CreateGenre.cs
+++ src/FC.Codeflix.Catalog.Application/UseCases/Genre/CreateGenre/CreateGenre.cs
@@ -2,6 +2,8 @@
 using FC.Codeflix.Catalog.Application.Interfaces;
 using FC.Codeflix.Catalog.Application.UseCases.Genre.Common;
 using FC.Codeflix.Catalog.Domain.Repository;
+using MediatR;
+using System.Threading;
 using DomainEntity = FC.Codeflix.Catalog.Domain.Entity;
 
 namespace FC.Codeflix.Catalog.Application.UseCases.Genre.CreateGenre;
@@ -13,7 +15,7 @@ public class CreateGenre : ICreateGenre
     private readonly IUnitOfWork _unitOfWork;
 
     public CreateGenre(
-        IGenreRepository genreRepository, 
+        IGenreRepository genreRepository,
         IUnitOfWork unitOfWork,
         ICategoryRepository categoryRepository
     ) {
@@ -23,35 +25,43 @@ public class CreateGenre : ICreateGenre
     }
 
     public async Task<GenreModelOutput> Handle(
-        CreateGenreInput request, 
+        CreateGenreInput request,
         CancellationToken cancellationToken)
     {
         var genre = new DomainEntity.Genre(
-            request.Name, 
+            request.Name,
             request.IsActive
         );
+
         if (request.CategoriesIds is not null)
         {
-            var idsInPersistence = await _categoryRepository
-                .GetIdsListByIds(
-                    request.CategoriesIds,
-                    cancellationToken
-                );
-
-            if (idsInPersistence.Count < request.CategoriesIds.Count)
-            {
-                var notFounds = request.CategoriesIds
-                    .FindAll(x => !idsInPersistence.Contains(x));
-                var notFoundIdsAsString = String.Join(", ", notFounds);
-                throw new RelatedAggregateException(
-                    $"Related category id (or ids) not found: {notFoundIdsAsString}"
-                );
-            }
+            await ValidateCategoriesIds(request, cancellationToken);
             request.CategoriesIds.ForEach(genre.AddCategory);
         }
-            
+
         await _genreRepository.Insert(genre, cancellationToken);
         await _unitOfWork.Commit(cancellationToken);
         return GenreModelOutput.FromGenre(genre);
+    }
+
+    private async Task ValidateCategoriesIds(
+        CreateGenreInput request,
+        CancellationToken cancellationToken)
+    {
+        var idsInPersistence = await _categoryRepository
+            .GetIdsListByIds(
+                request.CategoriesIds!,
+                cancellationToken
+            );
+
+        if (idsInPersistence.Count < request.CategoriesIds!.Count)
+        {
+            var notFounds = request.CategoriesIds
+                .FindAll(x => !idsInPersistence.Contains(x));
+            var notFoundIdsAsString = String.Join(", ", notFounds);
+            throw new RelatedAggregateException(
+                $"Related category id (or ids) not found: {notFoundIdsAsString}"
+            );
+        }
     }
 }
