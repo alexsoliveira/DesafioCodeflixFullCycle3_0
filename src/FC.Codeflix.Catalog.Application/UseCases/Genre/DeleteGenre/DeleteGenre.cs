b6082a45c0aa03402a36899177684e058f48856d
alexsoliveira <alexsoliveira@gmail.com>
2025-04-25 13:48:40 -0300

    Teste do DeleteGenre e Implementação do DeleteGenre

---
+++ src/FC.Codeflix.Catalog.Application/UseCases/Genre/DeleteGenre/DeleteGenre.cs
@@ -0,0 +1,30 @@
+﻿using FC.Codeflix.Catalog.Application.Interfaces;
+using FC.Codeflix.Catalog.Domain.Repository;
+
+namespace FC.Codeflix.Catalog.Application.UseCases.Genre.DeleteGenre
+{
+    public class DeleteGenre
+        : IDeleteGenre
+    {
+        private readonly IGenreRepository _genericRepository;
+        private readonly IUnitOfWork _unitOfWork;
+
+        public DeleteGenre(
+            IGenreRepository genericRepository, 
+            IUnitOfWork unitOfWork)
+        {
+            _genericRepository = genericRepository;
+            _unitOfWork = unitOfWork;
+        }
+
+        public async Task Handle(DeleteGenreInput request, CancellationToken cancellationToken)
+        {
+            var genre = await _genericRepository.Get(
+                request.Id, 
+                cancellationToken
+            );
+            await _genericRepository.Delete(genre, cancellationToken);
+            await _unitOfWork.Commit(cancellationToken);            
+        }        
+    }
+}
---
+++ src/FC.Codeflix.Catalog.Application/UseCases/Genre/DeleteGenre/DeleteGenreInput.cs
@@ -0,0 +1,12 @@
+﻿using MediatR;
+
+namespace FC.Codeflix.Catalog.Application.UseCases.Genre.DeleteGenre
+{
+    public class DeleteGenreInput
+        :IRequest
+    {
+        public DeleteGenreInput(Guid id) => Id = id;
+
+        public Guid Id { get; set; }
+    }
+}
