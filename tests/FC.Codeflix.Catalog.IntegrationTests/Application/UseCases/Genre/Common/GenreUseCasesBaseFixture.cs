7bd9837d076044cf61df40cc38e9149f476bd187
alexsoliveira <alexsoliveira@gmail.com>
2025-05-15 17:52:30 -0300

    Setup do teste do use case GetGenre

---
+++ tests/FC.Codeflix.Catalog.IntegrationTests/Application/UseCases/Genre/Common/GenreUseCasesBaseFixture.cs
@@ -0,0 +1,9 @@
+﻿using FC.Codeflix.Catalog.IntegrationTests.Base;
+
+namespace FC.Codeflix.Catalog.IntegrationTests.Application.UseCases.Genre.Common
+{
+    public class GenreUseCasesBaseFixture 
+        : BaseFixture
+    {
+    }
+}
