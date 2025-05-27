6721b8589e4a1dd50fd26ff0c446daddb5befa40
alexsoliveira <alexsoliveira@gmail.com>
2025-05-27 19:26:57 -0300

    Teste integradodo UseCas GetGenre testando as relações

--- tests/FC.Codeflix.Catalog.IntegrationTests/Application/UseCases/Genre/Common/GenreUseCasesBaseFixture.cs
+++ tests/FC.Codeflix.Catalog.IntegrationTests/Application/UseCases/Genre/Common/GenreUseCasesBaseFixture.cs
@@ -1,4 +1,5 @@
-﻿using FC.Codeflix.Catalog.IntegrationTests.Base;
+﻿using FC.Codeflix.Catalog.Domain.SeedWork.SearchableRepository;
+using FC.Codeflix.Catalog.IntegrationTests.Base;
 using DomainEntity = FC.Codeflix.Catalog.Domain.Entity;
 
 namespace FC.Codeflix.Catalog.IntegrationTests.Application.UseCases.Genre.Common
@@ -31,5 +32,63 @@ namespace FC.Codeflix.Catalog.IntegrationTests.Application.UseCases.Genre.Common
                 .Range(1, count)
                 .Select(_ => GetExampleGenre())
                 .ToList();
+
+        public string GetValidCategoryName()
+        {
+            var categoryName = "";
+
+            while (categoryName.Length < 3)
+                categoryName = Faker.Commerce.Categories(1)[0];
+
+            if (categoryName.Length > 255)
+                categoryName = categoryName[..255];
+
+            return categoryName;
+        }
+
+        public string GetValidCategoryDescription()
+        {
+            var categoryDescription =
+                Faker.Commerce.ProductDescription();
+
+            if (categoryDescription.Length > 10_000)
+                categoryDescription
+                    = categoryDescription[..10_000];
+
+            return categoryDescription;
+        }
+
+        public DomainEntity.Category GetExampleCategory()
+        => new(
+            GetValidCategoryName(),
+            GetValidCategoryDescription(),
+            GetRandomBoolean()
+        );
+
+        public List<DomainEntity.Category> GetExampleCategoriesList(int length = 10)
+            => Enumerable.Range(1, length)
+            .Select(_ => GetExampleCategory()).ToList();
+
+        public List<DomainEntity.Genre> CloneGenresListOrdered(
+            List<DomainEntity.Genre> genresList,
+            string orderBy,
+            SearchOrder order
+        )
+        {
+            var listClone = new List<DomainEntity.Genre>(genresList);
+            var orderedEnumerable = (orderBy.ToLower(), order) switch
+            {
+                ("name", SearchOrder.Asc) => listClone.OrderBy(x => x.Name)
+                    .ThenBy(x => x.Id),
+                ("name", SearchOrder.Desc) => listClone.OrderByDescending(x => x.Name)
+                    .ThenByDescending(x => x.Id),
+                ("id", SearchOrder.Asc) => listClone.OrderBy(x => x.Id),
+                ("id", SearchOrder.Desc) => listClone.OrderByDescending(x => x.Id),
+                ("createdat", SearchOrder.Asc) => listClone.OrderBy(x => x.CreatedAt),
+                ("createdat", SearchOrder.Desc) => listClone.OrderByDescending(x => x.CreatedAt),
+                _ => listClone.OrderBy(x => x.Name).ThenBy(x => x.Id),
+            };
+            return orderedEnumerable.ToList();
+        }
     }
 }
--- tests/FC.Codeflix.Catalog.IntegrationTests/Application/UseCases/Genre/GetGenre/GetGenreTest.cs
+++ tests/FC.Codeflix.Catalog.IntegrationTests/Application/UseCases/Genre/GetGenre/GetGenreTest.cs
@@ -1,4 +1,5 @@
 ﻿using FC.Codeflix.Catalog.Application.Exceptions;
+using FC.Codeflix.Catalog.Infra.Data.EF.Models;
 using FC.Codeflix.Catalog.Infra.Data.EF.Repositories;
 using FluentAssertions;
 using Xunit;
@@ -49,11 +50,50 @@ namespace FC.Codeflix.Catalog.IntegrationTests.Application.UseCases.Genre.GetGen
             var useCase = new UseCase.GetGenre(genreRepository);
             var input = new UseCase.GetGenreInput(randomGuid);
 
-            var action = async () 
+            var action = async ()
                 => await useCase.Handle(input, CancellationToken.None);
 
             await action.Should().ThrowAsync<NotFoundException>()
                 .WithMessage($"Genre '{randomGuid}' not found.");
+        }
+
+        [Fact(DisplayName = nameof(GetGenreWithCategoryRelations))]
+        [Trait("Integration/Application", "GetGenre - Use Cases")]
+        public async Task GetGenreWithCategoryRelations()
+        {
+            var genresExampleList = _fixture.GetExampleListGenres(10);
+            var categoriesExampleList = _fixture.GetExampleCategoriesList(5);
+            var expectedGenre = genresExampleList[5];
+            categoriesExampleList.ForEach(
+                category => expectedGenre.AddCategory(category.Id)
+            );                
+            var dbArrangeContext = _fixture.CreateDbContext();
+            await dbArrangeContext.Categories.AddRangeAsync(categoriesExampleList);
+            await dbArrangeContext.Genres.AddRangeAsync(genresExampleList);
+            await dbArrangeContext.GenresCategories.AddRangeAsync(
+                expectedGenre.Categories.Select(
+                    categoryId => new GenresCategories(
+                        categoryId,
+                        expectedGenre.Id
+                    )
+                )
+            );
+            await dbArrangeContext.SaveChangesAsync();
+            var genreRepository = new GenreRepository(_fixture.CreateDbContext(true));
+            var useCase = new UseCase.GetGenre(genreRepository);
+            var input = new UseCase.GetGenreInput(expectedGenre.Id);
+
+            var output = await useCase.Handle(input, CancellationToken.None);
+
+            output.Should().NotBeNull();
+            output.Id.Should().Be(expectedGenre.Id);
+            output.Name.Should().Be(expectedGenre.Name);
+            output.IsActive.Should().Be(expectedGenre.IsActive);
+            output.CreatedAt.Should().Be(expectedGenre.CreatedAt);
+            output.Categories.Should().HaveCount(expectedGenre.Categories.Count);
+            output.Categories.ToList().ForEach(
+                id => expectedGenre.Categories.Should().Contain(id)
+            );
         }
     }
 }
